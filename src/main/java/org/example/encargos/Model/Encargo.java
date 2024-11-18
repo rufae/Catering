ba95d2b6952cbac3d9193443574671608fc46d5a
rafael <rcastanoblanca@safareyes.es>
2024-11-18 13:42:39 +0100

    falta el login

--- src/main/java/org/example/encargos/Model/Encargo.java
+++ src/main/java/org/example/encargos/Model/Encargo.java
@@ -2,6 +2,8 @@ package org.example.encargos.Model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.LocalDate;
 
@@ -19,10 +21,12 @@ public class Encargo {
 
     @ManyToOne
     @JoinColumn(name = "cliente_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Cliente cliente;
 
     @ManyToOne
     @JoinColumn(name = "menu_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Menu menu;
 
     @Column(name = "fecha_encargo")
